00177e939084eb52ce04808cecf671a7690ce051
Daniel Tkach <danieltkach@gmail.com>
2021-04-16 00:58:32 -0300

    added age and gender

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ Promise.all([
   faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
   faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
   faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+  faceapi.nets.ageGenderNet.loadFromUri("/models"),
 ]).then(startVideo);
 
 function startVideo() {
@@ -24,11 +25,19 @@ video.addEventListener("play", () => {
     const detections = await faceapi
       .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
       .withFaceLandmarks()
+      .withAgeAndGender()
       .withFaceExpressions();
     const resizedDetections = faceapi.resizeResults(detections, displaySize);
     canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
     faceapi.draw.drawDetections(canvas, resizedDetections);
     faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
     faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+    resizedDetections.forEach(detection => {
+      const box = detection.detection.box;
+      const drawBox = new faceapi.draw.DrawBox(box, {
+        label: Math.round(detection.age) + " year old " + detection.gender,
+      });
+      drawBox.draw(canvas);
+    });
   }, 100);
 });
